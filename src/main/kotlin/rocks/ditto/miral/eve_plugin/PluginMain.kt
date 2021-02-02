337d3b8dbfa22440de1e16073b1cb2c5a7f92dcc
MurphyWhite <white@ditto.rocks>
2021-02-02 17:10:42 +0800

    建立项目结构

---
+++ src/main/kotlin/rocks/ditto/miral/eve_plugin/PluginMain.kt
@@ -0,0 +1,19 @@
+package rocks.ditto.miral.eve_plugin.rocks.ditto.miral.eve_plugin;
+
+import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescription
+import net.mamoe.mirai.console.plugin.jvm.KotlinPlugin
+
+
+object PluginMain : KotlinPlugin(
+    JvmPluginDescription("rocks.ditto.my-plugin", "1.0")
+) {
+
+    override fun onEnable() {
+
+
+
+    }
+
+    override fun onDisable() {
+    }
+}
---
+++ src/main/kotlin/rocks/ditto/miral/eve_plugin/command/JitaBuyCommand.kt
@@ -0,0 +1,4 @@
+package rocks.ditto.miral.eve_plugin.command
+
+class JitaBuyCommand {
+}
