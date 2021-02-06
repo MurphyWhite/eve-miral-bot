fd5193923a5e05e6208ffaf7980c93499931b451
MurphyWhite <white@ditto.rocks>
2021-02-06 23:31:49 +0800

    使用rawCommand的方式暂时解决物品英文名有空格的情况

--- src/main/kotlin/rocks/ditto/miral/eve_plugin/command/JitaCommand.kt
+++ src/main/kotlin/rocks/ditto/miral/eve_plugin/command/JitaCommand.kt
@@ -1,24 +1,37 @@
 package rocks.ditto.miral.eve_plugin.command
 
+import net.mamoe.mirai.console.MiraiConsole
 import net.mamoe.mirai.console.command.CommandSender
 import net.mamoe.mirai.console.command.CompositeCommand
+import net.mamoe.mirai.console.command.RawCommand
 import net.mamoe.mirai.console.command.SimpleCommand
+import net.mamoe.mirai.message.data.MessageChain
+import net.mamoe.mirai.message.data.content
+import net.mamoe.mirai.utils.MiraiLogger
 import rocks.ditto.miral.eve_plugin.rocks.ditto.miral.eve_plugin.PluginMain
 import rocks.ditto.miral.eve_plugin.service.EveService
+import java.lang.StringBuilder
 
 /**
  * jita 命令
  * 获取jita的买单和卖单
  */
-object JitaCommand : SimpleCommand(
+object JitaCommand : RawCommand(
     PluginMain,
     "jita",
     "j",
     description = "jita"
 ) {
-    @Handler
+    private val logger: MiraiLogger = MiraiConsole.createLogger(this.javaClass.name)
+
     suspend fun CommandSender.handle(msg:String) {
         var msg = EveService.fetchJita(msg)
+        sendMessage(msg)
+    }
+
+    override suspend fun CommandSender.onCommand(args: MessageChain) {
+        var text = args.joinToString(" ")
+        var msg = EveService.fetchJita(text)
         sendMessage(msg)
     }
 }
